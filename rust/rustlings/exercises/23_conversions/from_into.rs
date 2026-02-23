0afa105b85e67ee5fa8d545d8713d611df71738b
Joao L Souza Jr <joaolourencojr@gmail.com>
2026-02-23 10:06:34 -0300

    finishing rustling :D

--- rust/rustlings/exercises/23_conversions/from_into.rs
+++ rust/rustlings/exercises/23_conversions/from_into.rs
@@ -34,7 +34,17 @@ impl Default for Person {
 // 5. Parse the second element from the split operation into a `u8` as the age.
 // 6. If parsing the age fails, return the default of `Person`.
 impl From<&str> for Person {
-    fn from(s: &str) -> Self {}
+    fn from(s: &str) -> Self {
+        let value: Vec<_> = s.split(',').collect();
+        if value.len() == 2 
+            && !value[0].is_empty() 
+            && !value[1].is_empty()
+            && value[1].parse::<u8>().is_ok()
+            {
+            return Person { name: value[0].to_string(), age: value[1].parse::<u8>().unwrap()};
+        }
+        Person::default()
+    }
 }
 
 fn main() {
--- rust/rustlings/exercises/23_conversions/from_str.rs
+++ rust/rustlings/exercises/23_conversions/from_str.rs
@@ -41,7 +41,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {}
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let params: Vec<_> = s.split(',').collect();
+
+        if params.len() != 2 {
+            return  Err(ParsePersonError::BadLen);
+        }
+
+        if params[0].is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let name = params[0].to_string();
+        let age = params[1].parse::<u8>().map_err(ParsePersonError::ParseInt)?;
+
+        Ok(Person { name, age })
+    }
 }
 
 fn main() {
